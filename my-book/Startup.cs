650b97ad0edc0f8efc5d1abad2405936586f7caf
kowsalyasekarstudy <kowsalyasekarstudy@gmail.com>
2022-02-27 16:23:03 +0530

    After removing whether controller

--- my-book/Startup.cs
+++ my-book/Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using my_book.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,9 +18,11 @@ namespace my_book
 {
     public class Startup
     {
+        public string ConnectionString { get; set; }
         public Startup(IConfiguration configuration)
         {
             Configuration = configuration;
+            ConnectionString = configuration.GetConnectionString("DefaultConnectionString");
         }
 
         public IConfiguration Configuration { get; }
@@ -28,6 +32,8 @@ namespace my_book
         {
 
             services.AddControllers();
+            //Configure DBContext with sql
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(ConnectionString));
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "my_book", Version = "v1" });
@@ -54,6 +60,8 @@ namespace my_book
             {
                 endpoints.MapControllers();
             });
+        
+            AppDbInitializer.seed(app);
         }
     }
 }
